492b0727e943102abed7c36cf5fe9fe2e67ad52e
cooldev900 <cooldev900@gmail.com>
2024-12-26 14:30:17 -0500

    create Initialize struct

--- programs/step-staking-test/src/lib.rs
+++ programs/step-staking-test/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint, Token, TokenAccount};
 
 declare_id!("7ZYww9sNB9iJKYCh3LefK2RiczAHddTLZ8dz9SGSfXzJ");
 
@@ -17,4 +18,26 @@ pub mod step_staking_test {
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        address = constants::STEP_TOKEN_MINT_PUBKEY.parse::<Pubkey>().unwrap(),
+    )]
+    pub token_mint: Box<Account<'info, Mint>>,
+
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>,
+
+    #[account(
+        init,
+        payer = initializer,
+        token::mint = token_mint,
+        token::authority = token_vault,
+        seeds = [constants::STEP_TOKEN_MINT_PUBKEY.parse::<Pubkey>().unwrap().as_ref()],
+        bump,
+    )]
+    pub token_vault: Box<Account<'info, TokenAccount>>,
+}
