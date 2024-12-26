266a6e1f5cd52a91f921ec9ce1444a6884d7f22d
cooldev900 <cooldev900@gmail.com>
2024-12-26 13:38:01 -0500

    add mint accounts for step and xStep token

--- programs/step-staking-test/src/lib.rs
+++ programs/step-staking-test/src/lib.rs
@@ -2,6 +2,11 @@ use anchor_lang::prelude::*;
 
 declare_id!("7ZYww9sNB9iJKYCh3LefK2RiczAHddTLZ8dz9SGSfXzJ");
 
+pub mod constants {
+    pub const STEP_TOKEN_MINT_PUBKEY: &str = "5ehg7BoeVn1sjdKMV613GybX5mz2mYjJdKfZWrpz3CHb";
+    pub const X_STEP_TOKEN_MINT_PUBKEY: &str = "GqRfdTLC2VnNaQqD3ckit2dox6TfDeMaHc4MM9F5mJAF";
+}
+
 #[program]
 pub mod step_staking_test {
     use super::*;
